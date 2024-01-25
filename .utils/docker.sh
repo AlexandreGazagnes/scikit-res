645df94a44cabcc9b38f8784df9fc48677936e4d
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-25 16:14:30 +0100

    update utils

--- .utils/docker.sh
+++ .utils/docker.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 # docker base
-docker build -f ./utils/Dockerfile.base -t skres:base .
+docker build -f .utils/Dockerfile.base -t skres:base .
 
 # docker build
-docker build --no-cache -f ./utils/Dockerfile -t skres:latest .
+docker build --no-cache -f .utils/Dockerfile -t skres:latest .
 
 # docker run
 # docker run -ti skres:latest /bin/bash
